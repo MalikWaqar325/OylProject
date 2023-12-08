ec8cf5f681bd23db64a1f9f2bc3794a6cb5167ee
MalikWaqar325 <148744837+MalikWaqar325@users.noreply.github.com>
2023-12-08 16:25:12 +0500

    oyl

---
+++ App.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+import RootNavigation from './src/navigation/RootNavigation';
+import { AuthProvider } from './src/navigation/AuthProvider';
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <RootNavigation />
+      
+    </AuthProvider>
+  );
+};
+
+export default App;
